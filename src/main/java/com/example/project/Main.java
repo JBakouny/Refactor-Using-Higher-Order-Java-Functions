200933e353fe338bf4a81733d18e9db66f63128d
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2022-11-04 11:53:28 +0200

    Refactor using Higher-Order fucntions

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -5,40 +5,40 @@ import org.jetbrains.annotations.NotNull;
 import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+
 public class Main {
 
     static int sum(List<Integer> xs) {
-        int res = 0;
-        for (Integer x : xs) {
-            res += x;
-        }
-        return res;
+        return foldLeft(xs, 0, Integer::sum);
     }
 
     static int product(List<Integer> xs) {
-        int res = 1;
-        for (Integer x : xs) {
-            res *= x;
-        }
-        return res;
+        return foldLeft(xs, 1, (z, x) -> z * x);
     }
 
     static @NotNull List<Integer> multByConstant(@NotNull List<Integer> xs, int coef) {
-        List<Integer> res = new ArrayList<Integer>();
-        for (Integer x : xs) {
-            res.add(x * coef);
+        return map(xs, x -> x * coef);
+    }
+
+    static @NotNull List<Integer> addConstant(@NotNull List<Integer> xs, int coef) {
+        return map(xs, x -> x + coef);
+    }
+
+    static <A, B> List<B> map(List<A> xs, Function<A, B> f) {
+        List<B> l = new ArrayList<>();
+        for (A x: xs) {
+            l.add(f.apply(x));
+        }
+        return l;
+    }
+
+    static <Z, T> Z foldLeft(List<T> xs, Z zero, BiFunction<Z, T, Z> op) {
+        Z res = zero;
+        for (T x: xs) {
+            res = op.apply(res, x);
         }
         return res;
     }
-
-    static List<Integer> addConstant(List<Integer> xs, int coef) {
-        List<Integer> res = new ArrayList<Integer>();
-        for (Integer x : xs) {
-            res.add(x + coef);
-        }
-        return res;
-    }
-
 
 
 }
